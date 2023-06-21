7d46c69629fe4f060f5787cc4346dfa59fa5d8b2
Prince Kumar <kumarprince1407@gmail.com>
2023-06-21 23:19:55 +0530

    Added initial commit

---
+++ SnakeAndLadder/src/com/bridgelabz/day4assignment/SnakeAndLadder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.day4assignment;
+
+import java.util.*;
+
+public class SnakeAndLadder {
+    public static void main(String args[]){
+        Random random = new Random();
+        System.out.println("Welcome to the Snake and Ladder game!");
+        int start=0;
+        int p1Position=start;
+        System.out.println("Currently, the player P1 is at position - "+p1Position);
+    }
+}
+/*
+Snake and Ladder
+game played with
+single player at start
+position 0
+* */
